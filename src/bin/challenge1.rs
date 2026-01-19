9a77d755be11a664fc2e1ae7df5bdef37a42facf
Torrahp <toto301011@gmail.com>
2026-01-19 15:58:38 +0700

    Challenge1 FIZZBUZZ

---
+++ src/bin/challenge1.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+fn main() {
+    println!("Enter the desired numerical value.");
+
+    let mut number_input = ระString::new(); 
+
+    io::stdin()
+        .read_line(&mut number_input)
+        .expect("Failed to read line");
+
+    let number: i32 =number_input
+        .trim()
+        .parse()
+        .expect("Please type a number!");
+
+    for i in 1..=number{
+        fizzbuzz(i);
+    }
+}
+
+/*fn fizzbuzz(n: i32){
+    if n % 3 == 0 && n % 5 == 0{
+        println!("FizzBuzz");
+    }else if n % 3 ==0 {
+        println!("Fizz");
+    }else if n % 5 ==0 {
+        println!("Buzz");
+    }else{
+        println!("{}", n)
+    }
+}*/
+
+fn fizzbuzz(n: i32){
+    match (n % 3, n % 5){
+        (0, 0) => println!("FizzBuzz"),
+        (0, _) => println!("Fizz"),
+        (_, 0) => println!("Buzz"),
+        _ => println!("{}", n),
+    }
+}
+
